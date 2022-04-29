237f4224bd77902d9d5a073e4936625f3cd3de5d
Qamber Hasnain <63412921+Qamberhasnain@users.noreply.github.com>
2022-04-30 03:15:46 +0530

    Update RunTree.py

--- RunTree.py
+++ RunTree.py
@@ -3,7 +3,7 @@ import pyautogui
 
 root= TreeApp.Tk()
 
-canvas1 = TreeApp.Canvas(root, width = 1080, height = 720)
+canvas1 = TreeApp.Canvas(root, width = 720, height = 480)
 canvas1.pack()
 
 def tree():  
