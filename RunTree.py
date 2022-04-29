0af80e754532f5d7e31e2e14df9644508132a97e
Qamberhasnain <qamber.qh786@gmail.com>
2022-04-30 02:21:15 +0530

    First commit

---
+++ RunTree.py
@@ -0,0 +1,19 @@
+import tkinter as TreeApp
+import pyautogui
+
+root= TreeApp.Tk()
+
+canvas1 = TreeApp.Canvas(root, width = 300, height = 300)
+canvas1.pack()
+
+def tree():  
+    label1 = TreeApp.Label(root, text= 'Tree command is executed! \n Please close this application.\n THANK YOU!', fg='green', font=('helvetica', 12, 'bold'))
+    canvas1.create_window(150, 200, window=label1)
+    pyautogui.hotkey('win','r')
+    pyautogui.write('tree')
+    pyautogui.press('enter')
+    
+button1 = TreeApp.Button(text='Execute Tree',command=tree, bg='brown',fg='white')
+canvas1.create_window(150, 150, window=button1)
+
+root.mainloop()
